743ffa2632ad9edea06f3c694dd4e9c4a82ff2e5
Ramon Reyes <reyesjunk@gmail.com>
2017-03-01 09:42:25 -0500

    add fizzbuzz and average

--- loop-aray-drills.js
+++ loop-aray-drills.js
@@ -1,3 +1,4 @@
+
 function min(numbers){
 	var i = 0
 	var smallest;
@@ -17,7 +18,7 @@ function max(numbers){
 	var j = 0;
 	var largest;
 		if (typeof smallest === 'undefined'){
-			largest = numbers[i];
+			largest = numbers[j];
 		}
 		while(j < numbers.length) {
 		if (numbers[j] > largest) {
@@ -30,3 +31,34 @@ function max(numbers){
 }
 min([4,7,2,1,-5,11]);
 max([4,7,2,1,0,11]);
+//AVERAGE
+function average(numberArray){
+	let av = 0;
+	numberArray.forEach(number => av+=number);
+	return av/numberArray.length;
+}
+console.log(average([4,5,6,7,8,8]));
+
+//FIZZ BUZZ
+function fizzBuzz(countTo){
+	let newArray = [];
+	for(let i = 1; i<=countTo; i++){
+		switch(true){
+			case i % 15 === 0:
+				newArray.push('fizzbuzz');
+				break;
+			case i % 3 === 0:
+				newArray.push('fizz');
+				break;
+			case i % 5 === 0:
+				newArray.push('buzz');
+				break;		
+			default:
+				newArray.push(i);
+				break;
+
+		}
+	};
+	return newArray;
+}
+console.log(fizzBuzz(20));
